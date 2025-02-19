d07838e5c852559be5ff34863ad529077c333a33
Darrell Bailey <darrelljsbailey@gmail.com>
2025-02-19 13:03:23 -0500

    Added name, job title, contact info and icons to UI as well as formatting.

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,14 +12,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,7 +23,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -44,9 +38,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             BusinessCardTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    BusinessCard(
-                        name = "Android"
-                    )
+                    BusinessCard()
                 }
             }
         }
@@ -54,7 +46,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BusinessCard(name: String, modifier: Modifier = Modifier) {
+fun BusinessCard(modifier: Modifier = Modifier) {
     Column (
         modifier = modifier
             .fillMaxSize()
@@ -134,6 +126,6 @@ fun ContactRow (contactInfo: String, contactIcon: Painter, modifier: Modifier = 
 @Composable
 fun GreetingPreview() {
     BusinessCardTheme {
-        BusinessCard("Android")
+        BusinessCard()
     }
 }
